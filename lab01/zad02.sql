f9d29ffa3a37ec4205c4bfd33db7825946e5e842
Katarzyna Winkler <kasia.winkler99@gmail.com>
2020-10-14 10:21:24 +0200

    lab01 - zad03

---
+++ lab01/zad02.sql
@@ -0,0 +1,5 @@
+insert into band(name, creationdate) values('Gnesis', '01/06/1967') ;
+insert into band(name, creationdate) values('Jon Bellion', '01/10/2010');
+insert into band(name, creationdate) values('Pink Floyd', '01/09/1964');
+
+select * from band
